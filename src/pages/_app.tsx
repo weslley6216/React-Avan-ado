98312eb45c9e6148176da5cb8de1603219393334
Weslley Campos <weslley.campos@rebase.com.br>
2022-02-17 10:03:21 -0300

    Add next pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import GlobalStyle from 'styles/global'
+
+import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,12 +10,13 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
-      <GlobalStyle />
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
